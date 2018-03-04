8003312c8745aed308a8cc419de27128583060ad
obionewilobi <wilfried.vandenberghe@gmail.com>
2018-03-04 18:19:39 +0100

    Add card for dashboard.

--- jpa/src/main/java/jpa/entity/Card.java
+++ jpa/src/main/java/jpa/entity/Card.java
@@ -39,6 +39,45 @@ public class Card {
         this.name = name;
     }
 
+    @Column(name = "CREATION_DATE", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+    public Line getLine() {
+        return line;
+    }
+
+    public void setLine(Line line) {
+        this.line = line;
+    }
+
+    @Column(name = "CURRENT_STAGE")
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY, optional = false)
+    public Stage getCurrentStage() {
+        return currentStage;
+    }
+
+    public void setCurrentStage(Stage currentStage) {
+        this.currentStage = currentStage;
+    }
+
+    @Column(name = "IND_DONE", nullable = false)
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Column(name = "DUE_DATE", nullable = true)
     public Date getDueDate() {
         return dueDate;
     }
--- jpa/src/main/java/jpa/entity/Dashboard.java
+++ jpa/src/main/java/jpa/entity/Dashboard.java
@@ -1,8 +1,9 @@
 package jpa.entity;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
 import java.util.zip.DataFormatException;
 
 /**
@@ -14,6 +15,54 @@ public class Dashboard {
     private long id;
     private String name;
     private Date creationDate;
-    private List<Stage> stages;
-    private List<Line> lines;
+    private Set<Stage> stages;
+    private Set<Line> lines;
+
+    @Id
+    @Column(name = "DASHBOARD_ID")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+@Column(name="NAME",unique = true, nullable=false)
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(name = "CREATION_DATE", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Set<Stage> getStages() {
+        return stages;
+    }
+
+    public void setStages(Set<Stage> stages) {
+        this.stages = stages;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+    public Set<Line> getLines() {
+        return lines;
+    }
+
+    public void setLines(Set<Line> lines) {
+        this.lines = lines;
+    }
 }
